e855360e53ff28f979b13a03cab530501246d548
Mitchell Wang <mitchell_wang@outlook.com>
2023-10-22 22:30:22 +1100

    cleaned up decryption

--- attachment_decryptor.py
+++ attachment_decryptor.py
@@ -3,21 +3,14 @@ from cryptography.fernet import Fernet
 import logging
 
 if __name__ == '__main__':
-    try:
-        os.mkdir("decrypted_files")
-        key = b'4kubp0WObXXqcfjLj42rWSyvPubOgCRNrhYsgb_P_pQ='
+    os.mkdir("decrypted_files")
+    key = b'4kubp0WObXXqcfjLj42rWSyvPubOgCRNrhYsgb_P_pQ='
 
-        for file_decrypt in os.listdir('files_recieved'):
-            if file_decrypt[:2] == 'e_':
-                with open(f'files_recieved/{file_decrypt}', 'rb') as encrypted_file:
-                    data = encrypted_file.read()
-                decrypted = Fernet(key).decrypt(data)
-                with open(f'decrypted_files/{file_decrypt[2:]}', 'ab') as regular_file:
-                    regular_file.write(decrypted)
-                # os.remove(f'files_recieved/{file_decrypt}')
-
-    except KeyboardInterrupt:
-        print('* Ctrl + C detected...program exiting *')
-
-    except Exception as ex:
-        logging.exception('* Error Occured: {}*'.format(ex))
+    for file_decrypt in os.listdir('files_recieved'):
+        if file_decrypt[:2] == 'e_':
+            with open(f'files_recieved/{file_decrypt}', 'rb') as encrypted_file:
+                data = encrypted_file.read()
+            decrypted = Fernet(key).decrypt(data)
+            with open(f'decrypted_files/{file_decrypt[2:]}', 'ab') as regular_file:
+                regular_file.write(decrypted)
+            # os.remove(f'files_recieved/{file_decrypt}')
